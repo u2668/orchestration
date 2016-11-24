eb293b3226e2b36ce27b9a4fea991ba286c225b8
Alexandr Shushunov <ashushunov@mail.ru>
2016-11-24 16:53:14 +0300

    fix depploy scripts

---
+++ run-remote.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+deployAddress="138.68.71.0"
+
+#copy scenario
+ssh root@${deployAddress} mkdir /opt/gotocanteen
+scp -r ./docker-compose.yml \
+  root@${deployAddress}:/opt/gotocanteen
+
+#stop previous  
+ssh root@${deployAddress} docker-compose -f \
+  /opt/gotocanteen/docker-compose.yml stop
+
+#run new
+ssh root@${deployAddress} docker-compose -f \
+  /opt/gotocanteen/docker-compose.yml up -d

419861fde782d9e616c4e58bf149a914c155d960
Alexandr Shushunov <ashushunov@mail.ru>
2016-11-25 00:23:45 +0300

    add images pulling

--- run-remote.sh
+++ run-remote.sh
@@ -7,9 +7,14 @@ ssh root@${deployAddress} mkdir /opt/gotocanteen
 scp -r ./docker-compose.yml \
   root@${deployAddress}:/opt/gotocanteen
 
+
 #stop previous  
 ssh root@${deployAddress} docker-compose -f \
   /opt/gotocanteen/docker-compose.yml stop
+
+#pull images
+ssh root@${deployAddress} docker-compose -f \
+  /opt/gotocanteen/docker-compose.yml pull
 
 #run new
 ssh root@${deployAddress} docker-compose -f \
